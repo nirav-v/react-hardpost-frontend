c73b4ea812821f4412545c9d5f7e535a32ce67da
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-07-13 13:34:01 -0700

    implement signup form as controlled component that successfully connects to backend and registers new user in database

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import ShopPage from "./components/pages/ShopPage";
+import SignUpForm from "./components/forms/SignUpForm";
 
 function App() {
-  useEffect(() => {
-    // Make the fetch request here
-    fetch("http://localhost:3000/shop")
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle the response data
-        console.log(data);
-      })
-      .catch((error) => {
-        // Handle any errors
-        console.error(error);
-      });
-  }, []);
-
-  return <div></div>;
+  return (
+    <div>
+      <SignUpForm />
+      <ShopPage />;
+    </div>
+  );
 }
 
 export default App;
